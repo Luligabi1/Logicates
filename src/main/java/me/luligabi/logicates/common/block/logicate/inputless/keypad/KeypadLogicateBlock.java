a71ab1111657d96eba931161eac681ab26443790
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2023-05-20 01:33:56 -0300

    Add Keypad Logicate's model, texture & direction logic

--- src/main/java/me/luligabi/logicates/common/block/logicate/inputless/keypad/KeypadLogicateBlock.java
+++ src/main/java/me/luligabi/logicates/common/block/logicate/inputless/keypad/KeypadLogicateBlock.java
@@ -1,16 +1,13 @@
 package me.luligabi.logicates.common.block.logicate.inputless.keypad;
 
 import me.luligabi.logicates.common.Logicates;
-import me.luligabi.logicates.common.block.logicate.Logicatable;
-import me.luligabi.logicates.common.block.logicate.LogicateType;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.*;
+import me.luligabi.logicates.common.block.logicate.inputless.InputlessLogicateBlock;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityTicker;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.state.StateManager;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
@@ -24,15 +21,14 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class KeypadLogicateBlock extends AbstractRedstoneGateBlock implements Logicatable, BlockEntityProvider {
+@SuppressWarnings("deprecation")
+public class KeypadLogicateBlock extends InputlessLogicateBlock implements BlockEntityProvider {
 
-    public KeypadLogicateBlock() {
-        super(FabricBlockSettings.copyOf(Blocks.REPEATER));
-    }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
@@ -46,13 +42,26 @@ public class KeypadLogicateBlock extends AbstractRedstoneGateBlock implements Lo
         return ActionResult.CONSUME;
     }
 
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new KeypadLogicateBlockEntity(pos, state);
+    }
+
+    @Override
+    public boolean onSyncedBlockEvent(BlockState state, World world, BlockPos pos, int type, int data) {
+        super.onSyncedBlockEvent(state, world, pos, type, data);
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        return blockEntity != null && blockEntity.onSyncedBlockEvent(type, data);
+    }
+
     @SuppressWarnings("ConstantConditions")
     public void powerOn(BlockState state, World world, BlockPos pos) {
         KeypadLogicateBlockEntity blockEntity = ((KeypadLogicateBlockEntity) world.getBlockEntity(pos));
 
         world.setBlockState(pos, state.with(POWERED, true), 3);
         world.updateNeighborsAlways(pos, state.getBlock());
-        world.scheduleBlockTick(pos, this, blockEntity.closingDelay*20);
+        world.scheduleBlockTick(pos, this, blockEntity.closingDelay * 20);
     }
 
     @Override
@@ -74,42 +83,22 @@ public class KeypadLogicateBlock extends AbstractRedstoneGateBlock implements Lo
     }
 
     @Override
-    protected int getUpdateDelayInternal(BlockState state) {
-        return 2;
-    }
-
-    @Nullable
-    @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new KeypadLogicateBlockEntity(pos, state);
-    }
-
-
-    @Override
-    public boolean onSyncedBlockEvent(BlockState state, World world, BlockPos pos, int type, int data) {
-        super.onSyncedBlockEvent(state, world, pos, type, data);
-        BlockEntity blockEntity = world.getBlockEntity(pos);
-        return blockEntity != null && blockEntity.onSyncedBlockEvent(type, data);
-    }
-
-    @Nullable
-    protected static <E extends BlockEntity, A extends BlockEntity> BlockEntityTicker<A> checkType(BlockEntityType<A> givenType, BlockEntityType<E> expectedType, BlockEntityTicker<? super E> ticker) {
-        return expectedType == givenType ? (BlockEntityTicker<A>) ticker : null;
-    }
-
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING, POWERED);
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        Direction direction = state.get(FACING);
+        BlockPos blockPos = pos.offset(direction.getOpposite());
+        BlockState blockState = world.getBlockState(blockPos);
+        return direction.getAxis().isHorizontal() && blockState.isSideSolidFullSquare(world, blockPos, direction);
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.fullCube();
-    }
-
-    @Override
-    public LogicateType getLogicateType() {
-        return LogicateType.INPUTLESS;
+        return switch(state.get(FACING)) {
+            case NORTH -> NORTH_SHAPE;
+            case SOUTH -> SOUTH_SHAPE;
+            case WEST -> WEST_SHAPE;
+            case EAST -> EAST_SHAPE;
+            default -> VoxelShapes.empty();
+        };
     }
 
     @Override
@@ -121,11 +110,14 @@ public class KeypadLogicateBlock extends AbstractRedstoneGateBlock implements Lo
 
     public static final int MIN_CLOSING_DELAY = 1;
     public static final int MAX_CLOSING_DELAY = 30;
-
     public static final Identifier KEYPAD_PASSWORD = Logicates.id("keypad_password");
     public static final Identifier KEYPAD_DELETE = Logicates.id("keypad_delete");
     public static final Identifier KEYPAD_CONFIRM = Logicates.id("keypad_confirm");
     public static final Identifier KEYPAD_TOGGLE_PASSWORD_RESET = Logicates.id("keypad_toggle_password_reset");
     public static final Identifier KEYPAD_CLOSING_DELAY = Logicates.id("keypad_closing_delay");
 
+    public static final VoxelShape NORTH_SHAPE = createCuboidShape(5.75, 5, 15, 10.25, 11, 16);
+    public static final VoxelShape SOUTH_SHAPE = createCuboidShape(5.75, 5, 0, 10.25, 11, 1);
+    public static final VoxelShape WEST_SHAPE = createCuboidShape(15, 5, 5.75, 16, 11, 10.25);
+    public static final VoxelShape EAST_SHAPE = createCuboidShape(0, 5, 5.75, 1, 11, 10.25);
 }
